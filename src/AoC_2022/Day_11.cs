96820f26eb6fd435979d32a46e1b43d9989ce637
Elliot Brown <elliot.b.93@gmail.com>
2022-12-12 19:35:55 +0000

    Bit of a mess but day 11 and 12 done

---
+++ src/AoC_2022/Day_11.cs
@@ -0,0 +1,351 @@
+﻿using System.Data;
+using System.Numerics;
+
+namespace AoC_2022;
+
+//namespace AoC_2022;
+
+//public class Day_11 : BaseDay
+//{
+//    private readonly Monkey[] _input;
+
+//    public Day_11()
+//    {
+//        _input = ParseInput();
+//    }
+
+//    public override ValueTask<string> Solve_1()
+//    {
+//        var monkies = _input;
+
+//        for (int r = 0; r < 10000; r++)
+//        {
+//            for (int i = 0; i < monkies.Length; i++)
+//            {
+//                var monkey = monkies[i];
+//                monkey.ProcessItems(monkies);
+//            }
+//        }
+
+//        var topTwoInspectors = monkies
+//            .OrderByDescending(m => m.InspectCount).Take(2).ToList();
+
+//        var monkeyBusiness = topTwoInspectors[0].InspectCount * topTwoInspectors[1].InspectCount;
+
+//        return new(monkeyBusiness.ToString());
+//    }
+
+//    public override ValueTask<string> Solve_2()
+//    {
+//        return new("".ToString());
+//    }
+
+//    private Monkey[] ParseInput()
+//    {
+//        //return File.ReadAllLines(InputFilePath).ToList();
+//        // Test input, expected 10605, no /3 2713310158
+//        return new[]
+//        {
+//            new Monkey(
+//               new List<int>(new[] { 79, 98 }),
+//                item => item * 19,
+//                item => item % 23 == 0 ? 2 : 3),
+//             new Monkey(
+//               new List<int>(new[] { 54, 65, 75, 74 }),
+//                item => item + 6,
+//                item => item % 19 == 0 ? 2 : 0),
+//             new Monkey(
+//               new List<int>(new[] { 79, 60, 97 }),
+//                item => item * item,
+//                item => item % 13 == 0 ? 1 : 3),
+//             new Monkey(
+//               new List<int>(new[] { 74 }),
+//                item => item + 3,
+//                item => item % 17 == 0 ? 0 : 1),
+//        };
+//        //return new[]
+//        //{
+//        //    new Monkey(
+//        //       new List<int>(new[] { 54, 61, 97, 63, 74 }),
+//        //        item => item * 7,
+//        //        item => item % 17 == 0 ? 5 : 3),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 61, 70, 97, 64, 99, 83, 52, 87 }),
+//        //        item => item + 8,
+//        //        item => item % 2 == 0 ? 7 : 6),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 60, 67, 80, 65 }),
+//        //        item => item * 13,
+//        //        item => item % 5 == 0 ? 1 : 6),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 61, 70, 76, 69, 82, 56 }),
+//        //        item => item + 7,
+//        //        item => item % 3 == 0 ? 5 : 2),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 79, 98 }),
+//        //        item => item + 2,
+//        //        item => item % 7 == 0 ? 0 : 3),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 72, 79, 55 }),
+//        //        item => item + 1,
+//        //        item => item % 13 == 0 ? 2 : 1),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 63 }),
+//        //        item => item + 4,
+//        //        item => item % 19 == 0 ? 7 : 4),
+//        //     new Monkey(
+//        //       new List<int>(new[] { 72, 51, 93, 63, 80, 86, 81 }),
+//        //        item => item * item,
+//        //        item => item % 11 == 0 ? 0 : 4),
+//        //};
+//    }
+
+//    private class Monkey 
+//    {
+//        private List<int> items;
+
+//        private Func<int, double> inspectOp;
+
+//        private Func<int, int> throwTest;
+
+//        public int InspectCount { get; set; }
+
+//        public Monkey(List<int> items, Func<int, double> inspectOp, Func<int, int> throwTest)
+//        {
+//            this.items = items;
+//            this.inspectOp = inspectOp;
+//            this.throwTest = throwTest;
+//        }
+
+//        public void CatchItem(int item) 
+//        {
+//            items.Add(item);
+//        }
+
+//        public void ProcessItems(Monkey[] allMonkeys) 
+//        {
+//            var monkiesToThrowTo = new List<Monkey>();
+
+//            for (int i = 0; i < items.Count(); i++)
+//            {
+//                UpdateWorryLevel(i);
+//                var monkeyTo = throwTest(items[i]);
+//                var monkeyToThrowTo = allMonkeys[monkeyTo];
+//                monkiesToThrowTo.Add(monkeyToThrowTo);
+//            }
+
+
+//            var item = items.ToArray();
+//            items.Clear();
+//            for (int i = 0; i < monkiesToThrowTo.Count; i++)
+//            {
+//                monkiesToThrowTo[i].CatchItem(item[i]);
+//            }
+//        }
+
+//        private void UpdateWorryLevel(int itemIndex)
+//        { 
+//            var newWorry = inspectOp(items[itemIndex]);
+//            InspectCount++;
+//           // var worryAfterRelief = (int)Math.Floor(newWorry / 3);
+//           // items[itemIndex] = worryAfterRelief;
+//            items[itemIndex] = (int)Math.Floor(newWorry);
+//        }
+//    }
+//}
+
+
+
+
+public class Day_11 : BaseDay
+{
+    private readonly Monkey[] _input;
+
+    public Day_11()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+       // var allDivisors = new int[] { 23, 19, 13, 17 };
+        var allDivisors = new int[] { 2, 3, 5, 7, 11, 19, 13, 17 };
+        int prod = 1;
+
+        foreach (int value in allDivisors)
+        {
+            prod *= value;
+        }
+
+        var monkies = _input;
+
+        checked
+        {
+            for (int r = 1; r <= 10000; r++)
+            {
+                var inspectCounts = new List<int>();
+
+                for (int i = 0; i < monkies.Length; i++)
+                {
+                    var monkey = monkies[i];
+                    monkey.ReduceWorry(prod);
+                    monkey.ProcessItems(monkies);
+                    inspectCounts.Add(monkey.InspectCount);
+                }
+
+                if (r % 20 == 0)
+                {
+                    Console.WriteLine("After Round " + r);
+                    for (int x = 0; x < inspectCounts.Count(); x++)
+                    {
+                        Console.WriteLine($"Monkey {x} inspect items {inspectCounts[x]} times");
+                    }
+                    var maxItemSize = monkies.SelectMany(m => m.GetAllItems()).Max();
+                    Console.WriteLine($"Max item size: {maxItemSize}");
+                }
+
+
+            }
+        }
+
+        var topTwoInspectors = monkies
+            .OrderByDescending(m => m.InspectCount).Take(2).ToList();
+
+        var monkeyBusiness = (BigInteger) topTwoInspectors[0].InspectCount * (BigInteger) topTwoInspectors[1].InspectCount;
+
+        return new(monkeyBusiness.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("".ToString());
+    }
+
+    private Monkey[] ParseInput()
+    {
+        //return File.ReadAllLines(InputFilePath).ToList();
+        // Test input, expected 10605, no /3 2713310158
+        //return new[]
+        //{
+        //    new Monkey(
+        //       new List<BigInteger>(new BigInteger[] { 79, 98 }),
+        //        item => item * 19,
+        //        item => item % 23 == 0 ? 2 : 3),
+        //     new Monkey(
+        //       new List<BigInteger>(new BigInteger[] { 54, 65, 75, 74 }),
+        //        item => item + 6,
+        //        item => item % 19 == 0 ? 2 : 0),
+        //     new Monkey(
+        //       new List<BigInteger>(new BigInteger[] { 79, 60, 97 }),
+        //        item => item * item,
+        //        item => item % 13 == 0 ? 1 : 3),
+        //     new Monkey(
+        //       new List<BigInteger>(new BigInteger[] { 74 }),
+        //        item => item + 3,
+        //        item => item % 17 == 0 ? 0 : 1),
+        //};
+        return new[]
+        {
+            new Monkey(
+               new List<BigInteger>(new BigInteger[] { 54, 61, 97, 63, 74 }),
+                item => item* 7,
+                item => item % 17 == 0 ? 5 : 3),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 61, 70, 97, 64, 99, 83, 52, 87 }),
+                item => item + 8,
+                item => item % 2 == 0 ? 7 : 6),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 60, 67, 80, 65 }),
+                item => item * 13,
+                item => item % 5 == 0 ? 1 : 6),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 61, 70, 76, 69, 82, 56 }),
+                item => item + 7,
+                item => item % 3 == 0 ? 5 : 2),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 79, 98 }),
+                item => item + 2,
+                item => item % 7 == 0 ? 0 : 3),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 72, 79, 55 }),
+                item => item + 1,
+                item => item % 13 == 0 ? 2 : 1),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 63 }),
+                item => item + 4,
+                item => item % 19 == 0 ? 7 : 4),
+             new Monkey(
+               new List<BigInteger>(new BigInteger[] { 72, 51, 93, 63, 80, 86, 81 }),
+                item => item * item,
+                item => item % 11 == 0 ? 0 : 4),
+        };
+    }
+
+    private class Monkey 
+    {
+        private List<BigInteger> items;
+
+        private Func<BigInteger, BigInteger> inspectOp;
+
+        private Func<BigInteger, int> throwTest;
+
+        public int InspectCount { get; set; }
+
+        public Monkey(List<BigInteger> items, Func<BigInteger, BigInteger> inspectOp, Func<BigInteger, int> throwTest)
+        {
+            this.items = items;
+            this.inspectOp = inspectOp;
+            this.throwTest = throwTest;
+        }
+
+        public void CatchItem(BigInteger item) 
+        {
+            items.Add(item);
+        }
+
+        public void ProcessItems(Monkey[] allMonkeys) 
+        {
+            var monkiesToThrowTo = new List<Monkey>();
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                UpdateWorryLevel(i);
+                var monkeyTo = throwTest(items[i]);
+                var monkeyToThrowTo = allMonkeys[monkeyTo];
+                monkiesToThrowTo.Add(monkeyToThrowTo);
+            }
+
+
+            var itemsToThrow = items.ToArray();
+            items.Clear();
+            for (int i = 0; i < monkiesToThrowTo.Count; i++)
+            {
+                monkiesToThrowTo[i].CatchItem(itemsToThrow[i]);
+            }
+        }
+
+        private void UpdateWorryLevel(int itemIndex)
+        {
+            var newWorry = inspectOp(items[itemIndex]);
+            InspectCount++;
+           // items[itemIndex] = (int)Math.Floor(newWorry / 3d);
+            items[itemIndex] = newWorry;
+        }
+
+        public List<BigInteger> GetAllItems()
+        {
+            return this.items;
+        }
+
+        public void ReduceWorry(int prod)
+        {
+            for (int i = 0; i < this.items.Count(); i++)
+            {
+                var newVal = this.items[i] % prod;
+                this.items[i] = newVal;
+            }
+        }
+    }
+}
+
+
---
+++ src/AoC_2022/Day_12.cs
@@ -0,0 +1,328 @@
+﻿using MoreLinq;
+using System.Data;
+
+namespace AoC_2022;
+
+public class Day_12 : BaseDay
+{
+    private readonly char[,] _input;
+
+    public Day_12()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var rowCount = _input.GetLength(0);
+        var colCount = _input.GetLength(1);
+
+        int startingR = 0;
+        int startingC = 0;
+
+        for (int r = 0; r < rowCount; r++)
+        {
+            for (int c = 0; c < colCount; c++)
+            {
+                var inputVal = _input[r, c];
+
+                if (inputVal == 'S')
+                {
+                    startingR = r;
+                    startingC = c;
+                }
+            }
+        }
+
+        var pathLength = GetShortestPath(startingR, startingC, false);
+
+        return new(pathLength.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var rowCount = _input.GetLength(0);
+        var colCount = _input.GetLength(1);
+
+        var paths = new List<int>();
+        var totalPoints = rowCount * colCount;
+        var totalChecked = 0;
+
+        for (int r = 0; r < rowCount; r++)
+        {
+            for (int c = 0; c < colCount; c++)
+            {
+                var inputVal = _input[r, c];
+
+                Console.WriteLine($"{totalChecked} / {totalPoints} checked");
+
+                if (inputVal == 'a')
+                {
+                    var pathLength = GetShortestPath(r, c, false);
+                    if (pathLength > 0)
+                    {
+                        paths.Add(pathLength);
+                        Console.WriteLine($"Lenth added: {pathLength} PathCount: " + paths.Count());
+                    }
+                }
+
+                totalChecked++;
+            }
+        }
+
+        return new(paths.Min().ToString());
+    }
+
+    private char[,] ParseInput()
+    {
+        var input = File.ReadAllLines(InputFilePath);
+
+        var rowCount = input.Length;
+        var colsCount = input[0].Length;
+
+        char[,] result = new char[rowCount, colsCount];
+        for (int r = 0; r < rowCount; r++)
+        {
+            var row = input[r];
+            for (int c = 0; c < colsCount; c++)
+            {
+                var v = row[c];
+                result[r, c] = v;
+            }
+        }
+
+
+        return result;
+    }
+
+    class Tile
+    {
+        public int R { get; set; }
+        public int C { get; set; }
+        public int Cost { get; set; }
+        public int Distance { get; set; }
+        public int CostDistance => Cost + Distance;
+        public Tile Parent { get; set; }
+
+        //The distance is essentially the estimated distance, ignoring walls to our target. 
+        //So how many tiles left and right, up and down, ignoring walls, to get there. 
+        public void SetDistance(int targetR, int targetC)
+        {
+            this.Distance = Math.Abs(targetR - R) + Math.Abs(targetC - C);
+        }
+    }
+
+    private static List<Tile> GetWalkableTiles(char[,] map, Tile currentTile, Tile targetTile)
+    {
+        var possibleTiles = new List<Tile>()
+    {
+        new Tile { R = currentTile.R, C = currentTile.C - 1, Parent = currentTile, Cost = currentTile.Cost + 1 },
+        new Tile { R = currentTile.R, C = currentTile.C + 1, Parent = currentTile, Cost = currentTile.Cost + 1},
+        new Tile { R = currentTile.R - 1, C = currentTile.C, Parent = currentTile, Cost = currentTile.Cost + 1 },
+        new Tile { R = currentTile.R + 1, C = currentTile.C, Parent = currentTile, Cost = currentTile.Cost + 1 },
+    };
+
+        possibleTiles.ForEach(tile => tile.SetDistance(targetTile.R, targetTile.C));
+
+        var maxR = map.GetLength(0) - 1;
+        var maxC = map.GetLength(1) - 1;
+
+        var currentTileValue = map[currentTile.R, currentTile.C];
+
+        if (currentTileValue == 'S')
+        {
+            currentTileValue = 'a'; 
+        }
+
+        return possibleTiles
+                .Where(tile => tile.R >= 0 && tile.R <= maxR)
+                .Where(tile => tile.C >= 0 && tile.C <= maxC)
+                .Where(tile => 
+                   (map[tile.R, tile.C] - currentTileValue < 2 && map[tile.R, tile.C] != 'E') //height diff
+                    || (map[tile.R, tile.C] == 'S')
+                    || (currentTileValue == 'z' && map[tile.R, tile.C] == 'E'))
+                .ToList();
+    }
+
+    private void PrintMap(char[,] map, List<Tile> tiles)
+    {
+        var rowCount = map.GetLength(0);
+        var colCount = map.GetLength(1);
+
+        for (int r = 0; r < rowCount; r++)
+        {
+            for (int c = 0; c < colCount; c++)
+            {
+                var tile = tiles.FirstOrDefault(t => t.R == r && t.C == c);
+
+                if (tile != null)
+                {
+                    Console.Write(tile.Distance);
+                }
+                else 
+                {
+                    var inputVal = map[r, c];
+
+                    Console.Write(inputVal);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private int GetShortestPath(int startingR, int startingC, bool printResult)
+    { 
+        var start = new Tile();
+        var finish = new Tile();
+
+        var rowCount = _input.GetLength(0);
+        var colCount = _input.GetLength(1);
+
+        for (int r = 0; r < rowCount; r++)
+        {
+            for (int c = 0; c < colCount; c++)
+            {
+                var inputVal = _input[r, c];
+
+                if (inputVal == 'E')
+                { 
+                    finish.R = r;
+                    finish.C = c;
+                }
+            }
+        }
+
+        start.R = startingR;
+        start.C = startingC;
+
+        start.SetDistance(finish.R, finish.C);
+
+        var activeTiles = new List<Tile>();
+        activeTiles.Add(start);
+        var visitedTiles = new List<Tile>();
+
+        var foo = 0;
+
+        while (activeTiles.Any())
+        {
+            var checkTile = activeTiles.OrderBy(x => x.CostDistance).First();
+            
+            //We found the destination and we can be sure (Because the the OrderBy above)
+            //That it's the most low cost option. 
+            var tile = checkTile;
+            if (checkTile.R == finish.R && checkTile.C == finish.C)
+            {
+                var inputCopyToModify = (char[,])_input.Clone();
+
+                if (!printResult) 
+                {
+                    var steps = 0;
+                    while (true)
+                    {
+                        tile = tile.Parent;
+                        if (tile == null) 
+                        {
+                            return steps;
+                        }
+                        steps++;
+                    }
+                }
+
+                Console.WriteLine("We are at the destination!");
+                Console.WriteLine("Retracing steps backwards...");
+
+                inputCopyToModify[tile.R, tile.C] = '*';
+                var stepsCount = 0;
+                while (true)
+                {
+                    Console.WriteLine($"{tile.R} : {tile.C}");
+                    // Point parent at current
+                    if (tile.Parent != null)
+                    {
+                        if (tile.Parent.R < tile.R)
+                        {
+                            inputCopyToModify[tile.Parent.R, tile.Parent.C] = 'v';
+                        }
+                        if (tile.Parent.R > tile.R)
+                        {
+                            inputCopyToModify[tile.Parent.R, tile.Parent.C] = '^';
+                        }
+                        if (tile.Parent.C < tile.C)
+                        {
+                            inputCopyToModify[tile.Parent.R, tile.Parent.C] = '>';
+                        }
+                        if (tile.Parent.C > tile.C)
+                        {
+                            inputCopyToModify[tile.Parent.R, tile.Parent.C] = '<';
+                        }
+                    }
+
+                    tile = tile.Parent;
+
+                    if (tile == null)
+                    {
+                        Console.WriteLine("Map looks like :");
+
+                        PrintMap(inputCopyToModify, new List<Tile>());
+
+                        Console.WriteLine("Done!");
+                        return stepsCount;
+                    }
+
+                    stepsCount++;
+                }
+              
+            }
+
+            visitedTiles.Add(checkTile);
+            activeTiles.Remove(checkTile);
+
+            var walkableTiles = GetWalkableTiles(_input, checkTile, finish);
+
+            foreach (var walkableTile in walkableTiles)
+            {
+                //We have already visited this tile so we don't need to do so again!
+                if (visitedTiles.Any(x => x.R == walkableTile.R && x.C == walkableTile.C))
+                    continue;
+
+                //It's already in the active list, but that's OK, maybe this new tile has a better value (e.g. We might zigzag earlier but this is now straighter). 
+                if (activeTiles.Any(x => x.R == walkableTile.R && x.C == walkableTile.C))
+                {
+                    var existingTile = activeTiles.First(x => x.R == walkableTile.R && x.C == walkableTile.C);
+                    if (existingTile.CostDistance > checkTile.CostDistance)
+                    {
+                        activeTiles.Remove(existingTile);
+                        activeTiles.Add(walkableTile);
+                    }
+                }
+                else
+                {
+                    //We've never seen this tile before so add it to the list. 
+                    activeTiles.Add(walkableTile);
+                }
+            }
+
+            foo++;
+
+            //Console.WriteLine("Active Tiles: " + foo.ToString());
+            //PrintMap(_input, activeTiles);
+
+            //Console.WriteLine("Visited Tiles: " + foo.ToString());
+            //PrintMap(_input, visitedTiles);
+        }
+
+
+        Console.WriteLine("No Path Found!");
+
+        return 0;
+    }
+}
+
+//          a - b   b - a  
+// 4 3 y    1       -1
+// 4 4 y    0       0
+// 4 5 y    -1      1
+// 4 6 n    -2      2
+
+
+
